23c018415616b6d80616746d0bf319cc06a9e1f7
Jarexxx <kerajon@gmail.com>
2020-05-09 17:04:03 +0200

    feature: search component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { CobiroButtonComponent } from './cobiro-button/cobiro-button.component';
+import { SearchComponent } from './search/search.component';
 
 
 
@@ -10,12 +11,14 @@ import { CobiroButtonComponent } from './cobiro-button/cobiro-button.component';
   declarations: [
     HeaderBarComponent,
     MainMenuComponent,
-    CobiroButtonComponent
+    CobiroButtonComponent,
+    SearchComponent
   ],
   exports: [
     HeaderBarComponent,
     MainMenuComponent,
-    CobiroButtonComponent
+    CobiroButtonComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
